8a23a285c99783321a73aad1d773d4f757b66efe
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-10 20:41:36 -0300

    termineiiiiiiii

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Write("Digite quantos o tempo (s) decorrido entre o raio e o trovão: ");
+double tempo = Convert.ToDouble(Console.ReadLine());
+
+double VelocidadeSom = RaioETrovao(340.29);
+Console.WriteLine($"a distancia {VelocidadeSom:N5}");
+
+double RaioETrovao(double VelocidadeSom)
+{
+    double distancia = VelocidadeSom * tempo / 1000;
+    return distancia;
+}
